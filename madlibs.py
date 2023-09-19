0c09800c3ccfca213639da2215c690e91c900985
taskdire <136046239+taskdire@users.noreply.github.com>
2023-09-19 14:05:22 -0400

    madlibs
    fewfwedewfwef

--- madlibs.py
+++ madlibs.py
@@ -1,5 +1,18 @@
-day_of_week = input("what day is it? ")
-if day_of_week == "Friday":
-    print("correct")
-else:
-    print("incorrect")
+food = input("food")
+name = input("name")
+adjective = input("adjective")
+noun = input("noun")
+verb1 = input("verb1")
+verb2 = input("verb2")
+verb3 = input("verb3")
+
+
+
+
+It was (food) day at school, and (NAME) was super (ADJECTIVE)___ for lunch. But when she went outside to eat, a
+___(NOUN)___ stole her ___(FOOD)___! ___(NAME)___ chased the
+___(NOUN)___ all over school. She ___(VERB1)___,
+___(VERB2)___, and ___(VERB3)___ through the playground. Then
+she tripped on her ___(NOUN)___ and the ___(NOUN)___ escaped!
+Luckily, ___(NAME)___’s friends were willing to share their
+___(FOOD)___ with her.
