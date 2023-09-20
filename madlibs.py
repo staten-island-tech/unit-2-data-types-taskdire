ccd05f252619f59c8f9ce01b4389e9d59fef560c
taskdire <136046239+taskdire@users.noreply.github.com>
2023-09-20 13:55:27 -0400

    Madlibs project
    psa. done with this

--- madlibs.py
+++ madlibs.py
@@ -1,18 +1,12 @@
-food = input("food")
-name = input("name")
-adjective = input("adjective")
-noun = input("noun")
-verb1 = input("verb1")
-verb2 = input("verb2")
-verb3 = input("verb3")
+food = input("Food: ")
+name = input("Name: ")
+adjective = input("Adjective: ")
+noun = input("Noun: ")
+verb1 = input("Verb1: ")
+verb2 = input("Verb2: ")
+verb3 = input("Verb3: ")
 
 
 
 
-It was (food) day at school, and (NAME) was super (ADJECTIVE)___ for lunch. But when she went outside to eat, a
-___(NOUN)___ stole her ___(FOOD)___! ___(NAME)___ chased the
-___(NOUN)___ all over school. She ___(VERB1)___,
-___(VERB2)___, and ___(VERB3)___ through the playground. Then
-she tripped on her ___(NOUN)___ and the ___(NOUN)___ escaped!
-Luckily, ___(NAME)___’s friends were willing to share their
-___(FOOD)___ with her.
+print(f"It was {food} day at school, and {name} was super {adjective} for lunch. But when she went outside to eat, a {noun} stole her {food}! {name} chased the {noun} all over school. She {verb1},{verb2}, and {verb3} through the playground. Then she tripped on her {noun} and the {noun} escaped! Luckily, {name}'s friends were willing to share their {food} with her.")
---
+++ project.py
@@ -0,0 +1,14 @@
+name = input("Name: ")
+noun = input("Noun: ")
+adjective = input("Adjective: ")
+verb1 = input("Verb1: ")
+verb2 = input("Verb2: ")
+verb3 = input("Verb3: ")
+food = input("Food: ")
+subject = input("subject: ")
+subject2 = input("subject2: ")
+subject3 = input("subject3: ")
+verb4 = input("verb4: ")
+
+
+print(f"On the first day of school {name} was {noun} on how {adjective} the school was. They wanted to {verb1} the school and never come {verb2}. But {name} {verb3} because their mother told them to. {name}'s mother had packed {food}, which was {name}'s favorite. {name} was suffering in {subject} but then {name} realized that he had {subject2} and {subject3} after {subject} So they {verb4} from school and went home.")
